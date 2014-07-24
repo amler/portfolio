5712c2e65f9ce4907c52a7d314aa6b6d96cfa957
Heather Amler <heatheramler@yahoo.com>
2014-07-24 13:48:50 -0400

    Added font awesome for a close modal icon

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$('.design-work').click(function(){
+	var src = $(this).find('img').attr('src');
+	console.log(src);
+	$('.modal-wrap').find('.detail').attr('src', src);
+	$('.modal-wrap').show();
+});
+
+
+
+$('.fa-times').click(function(){
+	$('.modal-wrap').hide();
+});
