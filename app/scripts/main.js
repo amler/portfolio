a013c67ccf3df0886ab45d0dda3cbc916897e9d8
Heather Amler <heatheramler@yahoo.com>
2014-07-24 22:25:32 -0400

    Revised slideshow to animate

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -61,39 +61,59 @@ var imageSlideshow = {
 	hideModal: function() { 
 		$('.modal-wrap').hide();
 	},
-	showModal:  function() {
-		$('.modal-wrap').show();
+	showModal: function() {
+		this.currentIndex = 0;
 		this.renderImages();
+		$('.modal-wrap').show();
 	},
 	slideshowNext: function() {
-		imageSlideshow.currentIndex +=1
-		var arrayTotal = imageSlideshow.imageArray.length;
-		if (imageSlideshow.currentIndex > (arrayTotal - 1)) {
-			imageSlideshow.currentIndex = 0
+		this.currentIndex +=1;
+		var arrayTotal = this.imageArray.length;
+		if (this.currentIndex > (arrayTotal - 1)) {
+			this.currentIndex = 0
 		}
-		imageSlideshow.renderImages();
+		this.fade();
 	},
 	slideshowPrev: function() {
-		imageSlideshow.currentIndex -=1
+		imageSlideshow.currentIndex -=1;
 		if (imageSlideshow.currentIndex <= 0) {
 			imageSlideshow.currentIndex = imageSlideshow.imageArray.length - 1;
 		}
-		imageSlideshow.renderImages();
+		
+		imageSlideshow.fade();
+	},
+	fade: function() {
+		$('.detail').animate({
+			opacity: 0,
+		}, 500, function() {
+			imageSlideshow.renderImages();
+		});
 	},
 	renderImages: function() {
 		for (var i = 0; i < this.imageArray.length; i++) {
 			if (i === this.currentIndex) {
-				$('.detail').attr('src', this.imageArray[i]);
+				var src = this.imageArray[i];
+				var img = new Image();
+				img.onload = function() {
+					$('.detail').attr('src', src);
+					$('.detail').animate({
+						opacity: 1,
+					}, 500, function() {
+						// Animation complete.
+					});
+				};
+				img.src = src;
 			}
 		}
 	}
 }
 
-$('.fa-chevron-right').on('click', imageSlideshow.slideshowNext);
-$('.fa-chevron-left').on('click', imageSlideshow.slideshowPrev);
-$('.fa-times').on('click', imageSlideshow.hideModal);
+$('.fa-chevron-right').on('click', function() { imageSlideshow.slideshowNext(); });
+$('.fa-chevron-left').on('click', function() { imageSlideshow.slideshowPrev(); });
+$('.fa-times').on('click', function() { imageSlideshow.hideModal(); });
 
-$('.design-work').click(function(){
+$('.design-work').click(function(event){
+	event.preventDefault();
 	var detailArray = $(this).data('images');
 	imageSlideshow.imageArray = detailArray;
 	imageSlideshow.showModal();
