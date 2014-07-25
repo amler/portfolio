07c958bd5fb8e9f68e34f57da2e3c4847340767b
Heather Amler <heatheramler@yahoo.com>
2014-07-25 15:13:21 -0400

    Revising content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,64 +1,9 @@
 'use strict';
 
-// $.fn.slideshow = function(options) {
-
-//  	var defaults = {
-// 		imageArray: options.length,
-// 		count: 0,
-// 		nextButton: $('.fa-chevron-right'),
-// 		prevButton: $('.fa-chevron-left')
-// 	};
-
-// 	nextButton.on('click', slideshowNext);
-// 	prevButton.on('click', slideshowPrev);
-	
-// 	function slideshowNext() {
-// 		settings.count +=1
-// 		if (settings.count > (settings.imageArray - 1)) {
-// 			settings.count = 0
-// 		}
-// 		settings.renderTestimonial();
-// 	};
-
-// 	function slideshowPrev() {
-// 		settings.count -=1
-// 		if (settings.count <= 0) {
-// 			settings.count = settings.imageArray - 1;
-// 		}
-// 		settings.renderTestimonial();
-// 	};
-	
-// 	function renderTestimonial () {
-// 		for (var i = 0; i < settings.imageArray; i++) {
-// 			if (i !== count) {
-// 				$('.detail').attr('src', settings.imageArray[i])
-// 			} else {
-// 				$('.detail').attr('src', settings.imageArray[i])
-// 			}
-// 		}
-// 	};	
-
-// 	var settings = $.extend( {}, defaults, options );
-// 	return this;
-// };
-
-
-// $('.design-work').click(function(){
-// 	var src = $(this).find('img').attr('src');
-// 	console.log(src);
-// 	$('.modal-wrap').find('.detail').attr('src', src);
-// 	$('.modal-wrap').show();
-// });
-
-// $('.fa-times').click(function(){
-// 	$('.modal-wrap').hide();
-// });
-
-
 var imageSlideshow = {
 	imageArray: [],
 	currentIndex: 0,
-	hideModal: function() { 
+	hideModal: function() {
 		$('.modal-wrap').hide();
 	},
 	showModal: function() {
@@ -70,7 +15,7 @@ var imageSlideshow = {
 		this.currentIndex +=1;
 		var arrayTotal = this.imageArray.length;
 		if (this.currentIndex > (arrayTotal - 1)) {
-			this.currentIndex = 0
+			this.currentIndex = 0;
 		}
 		this.fade();
 	},
@@ -106,11 +51,12 @@ var imageSlideshow = {
 			}
 		}
 	}
-}
+};
 
 $('.fa-chevron-right').on('click', function() { imageSlideshow.slideshowNext(); });
 $('.fa-chevron-left').on('click', function() { imageSlideshow.slideshowPrev(); });
 $('.fa-times').on('click', function() { imageSlideshow.hideModal(); });
+
 
 $('.design-work').click(function(event){
 	event.preventDefault();
